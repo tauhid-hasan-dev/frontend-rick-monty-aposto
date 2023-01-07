e378414c390daa70829ebf10adce2039e2fa99d6
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2023-01-07 22:04:20 +0300

    stasuts updated

--- src/components/grids/ResidentCard.js
+++ src/components/grids/ResidentCard.js
@@ -11,7 +11,13 @@ function ResidentCard({ resident }) {
         <div className="card card-side bg-slate-800  text-white shadow-xl gap-3  ">
             <figure className='mr-3 w-[45%] h-full'><img src={image} alt="Movie" /></figure>
             <div className=" flex flex-col justify-between  py-5">
-                <h2 className="card-title text-[18px]"><FaCircle className={status === 'Alive' ? "text-green-500" : "text-red-500"}></FaCircle>{resident.name}</h2>
+                <div className='flex items-center gap-2'>
+                    {status === 'Alive' && <FaCircle className={"text-green-500"}></FaCircle>}
+                    {status === 'Dead' && <FaCircle className={"text-red-500"}></FaCircle>}
+                    {status === 'unknown' && <FaCircle className={"text-yellow-500"}></FaCircle>}
+                    <h2 className="card-title text-[18px]">{resident.name}</h2>
+                </div>
+
                 <p className='text-sm'><span className='text-slate-300'>Status:</span> {status} </p>
                 <p className='text-sm'><span className='text-slate-300'>Type:</span> {type ? type : "Not Found"}</p>
                 <p className='text-sm'><span className='text-slate-300'>Gender:</span> {gender}</p>
