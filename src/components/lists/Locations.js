f05f0850e2f51d220ad8f4a5ed77ec3c0d815ca4
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2023-01-07 06:57:05 +0300

    first commit

---
+++ src/components/lists/Locations.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+const Locations = () => {
+
+   /*  useEffect(()=> {
+        this 
+    }) */
+    return (
+        <div>
+            Locations
+        </div>
+    );
+};
+
+export default Locations;
