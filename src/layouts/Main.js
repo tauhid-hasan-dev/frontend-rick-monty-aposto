7b734c90a2e74e7463a090b0dd7e5030060dd1d5
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2023-01-07 22:13:35 +0300

    routing updated for shared component

---
+++ src/layouts/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/shared/Footer';
+import Navbar from '../components/shared/Navbar';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
--- src/routes/router.js
+++ src/routes/router.js
@@ -3,15 +3,24 @@ import {
 } from "react-router-dom";
 import Residents from "../components/grids/Residents";
 import Locations from "../components/lists/Locations";
+import Main from "../layouts/Main";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Locations></Locations>,
-    },
-    {
-        path: "/residents/:id",
-        element: <Residents></Residents>,
-        loader: ({ params }) => fetch(`https://rickandmortyapi.com/api/location/${params.id}`)
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Locations></Locations>,
+            },
+            {
+                path: "/residents/:id",
+                element: <Residents></Residents>,
+                loader: ({ params }) => fetch(`https://rickandmortyapi.com/api/location/${params.id}`)
+            }
+
+        ]
     }
+
 ]);
